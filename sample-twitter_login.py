cad579f4c170f7f1262b9f1a710862666f030ae9
tfarmstrong309 <78559782+tfarmstrong309@users.noreply.github.com>
2021-02-04 15:41:07 -0500

    Add files via upload
    This is the source code for which it was tested

---
+++ sample-twitter_login.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+driver.get('https://twitter.com/login')
+XPATH_username = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input'
+XPATH_password = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input'
+XPATH_button = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div/span/span'
+time.sleep(6)
+# Username input line
+driver.find_element_by_xpath(XPATH_username).send_keys('t_f_armstrong')
+# Password
+driver.find_element_by_xpath(XPATH_password).send_keys('&3j}auM6+@;Z')
+#Login button
+driver.find_element_by_xpath(XPATH_button).click()
+
+
+
